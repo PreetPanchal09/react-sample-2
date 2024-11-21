c5f78882c0ab4e5b9c46571966024dfdda22cecb
PreetPanchal09 <Panchpre@gmail.com>
2024-11-20 19:47:28 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createElement, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [Board, setBoard] = useState(['','','','','','','','',''])
+  const [Player, setPlayer] = useState("X")
+
+  return (
+    <>
+      <h1>Tic-Tac-Toe</h1>
+      <div id="grid">
+        {Board.map(cell => {
+          Cell = createElement("p")
+          Cell.addEventListener(onclick, () => {
+            Cell.textContent = {Player}
+          })
+          return Cell
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
